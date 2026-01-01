76786affe99b6979d0dcf634699310bf96c827ab
senorhitesh <senorhitesh@gmail.com>
2026-01-01 22:31:22 +0530

    update day 1

--- src/day01-inventory-Manager/main.js
+++ src/day01-inventory-Manager/main.js
@@ -1,24 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Day 1: Inventory Analyzer</title>
-    <style>
-        body { font-family: sans-serif; padding: 2rem; }
-        .low-stock-alert { color: red; font-weight: bold; margin-top: 1rem;
-        list-style-type: none;
-        }
-    </style>
-</head>
-<body>
-    <h1>📦 Inventory System</h1>
-    <p>Check which items need reordering.</p>
-    
-    <button id="check-btn">Check Reorder Status</button>
-    
-    <div id="app"></div>
+import { getReorderList } from './logic.js';
 
-    <script type="module" src="./main.js"></script>
-</body>
-</html>
+// The Data (Usually this comes from an API, but we simulate it here)
+const inventory = [
+    { id: 1, name: "Laptop", price: 1000, stock: 5 },
+    { id: 2, name: "Mouse", price: 20, stock: 0 },
+    { id: 3, name: "Keyboard", price: 50, stock: 12 },
+    { id: 4, name: "Monitor", price: 300, stock: 2 },
+];
+
+const run = () => {
+     
+    let reOrderBtn = document.querySelector("#check-btn");
+    let appContainer = document.querySelector("#app");
+    
+    reOrderBtn.addEventListener("click", function(){
+    const reorderNames = getReorderList(inventory);
+    appContainer.innerHTML=``;
+    const ul = document.createElement('ul');
+
+    reorderNames.forEach(productName => {
+    const li = document.createElement("li");
+    li.textContent = `⚠️ Restock: ${productName}`;
+    li.classList.add("low-stock-alert");
+    ul.appendChild(li);   
+    });
+    
+    appContainer.appendChild(ul)
+    })
+
+};
+
+run();
