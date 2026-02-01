556cdd6d7c8d2a5bcd4aaba47dd10499448fc8ef
senorhitesh <senorhitesh@gmail.com>
2026-02-01 16:39:17 +0530

    feat(ui):- Quiz App

---
+++ src/day25-Quiz_App/src/script.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // 👈 Make sure this matches your file structure!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
