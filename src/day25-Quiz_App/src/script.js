3d98d62bac503e9d57f9c6d8f31938915d9d2544
senorhitesh <senorhitesh@gmail.com>
2026-02-01 18:39:20 +0530

    readme update

--- src/day25-Quiz_App/src/script.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.{html,js}"], // 👈 Make sure this matches your file structure!
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
